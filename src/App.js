e855a544dcd56721c32e1c5d46a45811298c418a
jasonwieczorek <wieczorek.jason@gmail.com>
2020-06-08 13:34:35 -0500

    formatted some stuff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import './App.css';
 
+// A react component, this is just a javascript function
 function App() {
+
+    //Do some stuff here
     const title = 'React';
 
-  return (//
-      <div>
-    <h1>Hello {title}</h1>
-      <label htmlFor="search">Search:</label>
-          <input id="search" type="text"/>
-      </div>
-  );
+    // variable types
+    // var - global or local scope, can be reassigned
+    // let - block scoped, can be updated but not re-declared
+    // const - cannot be updated or redeclared
+
+    // return some JSX stuff for display
+    return (
+        // This is JSX, NOT html
+        <div>
+            <h1>Hello {title}</h1>
+            <label htmlFor="search">Search:</label>
+            <input id="search" type="text"/>
+        </div>
+    );
 }
 
 export default App;
